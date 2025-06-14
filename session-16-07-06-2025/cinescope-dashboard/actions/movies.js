6a346bd8d2d34c0ee6b19fdc73b190f07ff0da12
Thamindu <myadd.ikman@gmail.com>
2025-06-14 20:41:40 +0530

    session initial commit 2025-06-07

---
+++ session-16-07-06-2025/cinescope-dashboard/actions/movies.js
@@ -0,0 +1,106 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { ObjectId } from "mongodb";
+
+// get all movies action
+export const getMovies = async () => {
+  try {
+    // using fetch API to get movies from the server
+    const response = await fetch("http://localhost:3000/api/v1/movies", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    if (response.status === 200) {
+      return await response.json();
+    } else {
+      console.log("No movies found!");
+      return undefined;
+    }
+  } catch (error) {
+    console.log("Error fetching movies:", error);
+    return undefined;
+  }
+};
+
+// Create Movie action
+
+export const createMovie = async (movie) => {
+  try {
+    const result = await db.collection("movies_n").insertOne(movie);
+
+    if (result.acknowledged) {
+      console.log(`A movie was inserted with the _id: ${result.insertedId}`);
+      return {
+        success: true,
+        message: "Movie created successfully!",
+      };
+    } else {
+      return undefined;
+    }
+  } catch {
+    console.log("Mongodb insert failed!");
+  }
+};
+
+
+// Update Movie action
+
+export const updateMovie = async (movieId, movieData) => {
+  try {
+    const result = await db.collection("movies_n").updateOne({_id : ObjectId.createFromHexString(movieId)}, {$set : movieData}, {upsert : true});
+    console.log(result);
+    
+    if (result.acknowledged) {
+      console.log(`A movie was update with the _id: ${result.insertedId}`);
+      return {
+        success: true,
+        message: "Movie updated successfully!",
+      };
+    } else {
+      return undefined;
+    }
+  } catch (error) {
+    console.error("MongoDB update failed:", error);
+    return {
+      success: false,
+      message: "MongoDB update failed",
+      error: error.message,
+    };
+  }
+};
+
+// Delete Movie action
+
+export const deleteMovie = async (movieId) => {
+  try {
+    const result = await db.collection("movies_n").deleteOne({_id : ObjectId.createFromHexString(movieId)});
+    console.log(result);
+    
+    if (result.acknowledged) {
+      console.log(`A movie was delete with the _id: ${result.insertedId}`);
+      return {
+        success: true,
+        message: "Movie delete successfully!",
+      };
+    } else {
+      return undefined;
+    }
+  } catch (error) {
+    console.error("MongoDB delete failed:", error);
+    return {
+      success: false,
+      message: "MongoDB delete failed",
+      error: error.message,
+    };
+  }
+};
---
+++ session-16-07-06-2025/cinescope-dashboard/app/admin/movies/movie-data.jsx
@@ -0,0 +1,39 @@
+import { db } from "@/lib/db";
+import MovieTable from "./movie-table";
+
+export default async function MovieData() {
+  try {
+    const movies = await db.collection("movies_n").find({}).limit(50).toArray();
+console.log("movies", movies);
+    if (movies.length > 0) {
+      const refinedMovies = movies.map((movie, key) => ({
+        id: movie._id.toString(),
+        title: movie.title,
+        year: movie.year,
+        plot: movie.plot,
+        rated: movie.rated,
+        genres: movie.genres,
+        poster: movie.poster,
+        imdb: movie.imdb,
+        runtime: movie.runtime,
+        status: movie.status ,
+        directors : movie.directors,
+
+        }));
+
+console.log("refinedMovies", refinedMovies);
+      return <MovieTable movies={refinedMovies} />;
+    } else {
+      throw new Error("No movies found in the database.");
+    }
+  } catch (error) {
+    console.log("Error fetching movies:", error);
+    return (
+      <div className="flex justify-center items-center h-[400px]">
+        <p className="text-destructive font-medium animate-pulse duration-1000">
+          No Movies Available!
+        </p>
+      </div>
+    );
+  }
+}
