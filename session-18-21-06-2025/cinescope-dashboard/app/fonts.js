f051c3825bd20ffc1baa5b72026daa9e99399ba6
Thamindu <myadd.ikman@gmail.com>
2025-06-27 20:47:17 +0530

    adding google fonts and using debounce

---
+++ session-18-21-06-2025/cinescope-dashboard/app/fonts.js
@@ -0,0 +1,9 @@
+import { Inter, Roboto_Mono } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const roboto_Mono = Roboto_Mono({
+  subsets: ["latin"],
+});
--- session-18-21-06-2025/cinescope-dashboard/app/layout.jsx
+++ session-18-21-06-2025/cinescope-dashboard/app/layout.jsx
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { inter } from "./fonts";
 import "./globals.css";
+inter
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "CineScope Movies Database - BO3",
@@ -20,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
